68624f5ea260e5002e66c08ddeb155cee4b65bbe
piotrnowoslawski <jdm-option@jdm-option.pl>
2018-03-20 01:37:29 +0100

    add rwd navigation - problems

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,13 +1,39 @@
 $(window).scroll(function(){
     if ($(this).scrollTop() > 50) {
-        $('.dynamic-nav').addClass('navigation-scroll');
-        $('.dynamic-nav').removeClass('desktop-navigation');
+        $('.dynamic-navigation').addClass('navigation-scroll');
+        $('.dynamic-navigation').removeClass('desktop-navigation');
         $('.dynamic-item').addClass('nav-item-scroll');
         $('.dynamic-item').removeClass('nav-item');
     } else {
-        $('.dynamic-nav').removeClass('navigation-scroll');
-        $('.dynamic-nav').addClass('desktop-navigation');
+        $('.dynamic-navigation').removeClass('navigation-scroll');
+        $('.dynamic-navigation').addClass('desktop-navigation');
         $('.dynamic-item').removeClass('nav-item-scroll');
         $('.dynamic-item').addClass('nav-item');
     }
 });
+
+
+$(window).width(function(){
+    if ($(this).width() < 960) {
+        $(".cross,.nav-items-list").hide();
+        $(".hamburger").click(function () {
+            $(".hamburger").hide();
+            $(".cross").show();
+            $(".nav-items-list").slideToggle("slow", function () {
+            });
+        });
+
+        $(".cross").click(function () {
+            $(".cross").hide();
+            $(".nav-items-list").slideToggle("slow", function () {
+                $(".hamburger").show();
+            });
+        });
+
+        $(".nav-items-list").click(function () {
+            $(".cross").hide();
+            $(".nav-items-list").slideToggle("slow", function () {
+                $(".hamburger").show();
+            });
+        });
+    }});
