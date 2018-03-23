9503e3e324a1d333fea169d71222d129b533143d
piotrnowoslawski <jdm-option@jdm-option.pl>
2018-03-23 20:49:12 +0100

    navigation upgrade 2.0

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,18 +2,17 @@ function handleNavigationEffect() {
     const scrollThreshold = 50;
     const $navigation = $('.desktop-navigation');
 
-    $(this).scrollTop > scrollThreshold ?
+    $(this).scrollTop() > scrollThreshold ?
         $navigation.addClass('navigation-scroll') :
         $navigation.removeClass('navigation-scroll');
 }
 
 $(window).on('scroll', handleNavigationEffect);
 
-$( ".cross,.navigation-items-list" ).hide();
 $( ".hamburger" ).click(function() {
     $( ".hamburger" ).hide();
-    $( ".cross" ).show();
     $( ".navigation-items-list" ).slideToggle( "slow", function() {
+        $( ".cross" ).show();
     });
 });
 
