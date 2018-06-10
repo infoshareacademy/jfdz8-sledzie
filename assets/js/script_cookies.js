a75a1232ba209258b89419f3ed44ece51ad0dd42
piotrnowoslawski <jdm-option@jdm-option.pl>
2018-06-10 15:46:10 +0200

    add cookies script

--- assets/js/script_cookies.js
+++ assets/js/script_cookies.js
@@ -1,40 +1,32 @@
-(function() {
-    var container = document.createElement('div'),
-        link = document.createElement('a'),
-        hulkContainer = document.createElement('div');
+var cookiesInitialize = function() {
+    var container = document.createElement('div');
+    var link = document.createElement('a');
+    var hulkContainer = document.createElement('div');
 
-
-    container.setAttribute('id', 'cookies_info');
-    container.setAttribute('class', 'cookie-alert');
+    container.id = 'cookies_info';
     container.innerHTML = '<h6>Ta strona wykorzystuje pliki cookie</h6>' +
         '<p>Używa informacji zapisanych za pomocą plików cookies w celu zapewnienia maksymalnej wygody w korzystaniu ze strony. Jeżeli wyrażasz zgodę na zapisywanie informacji zawartej w cookies kliknij na &bdquo;x&rdquo; w prawym górnym rogu tej informacji. Jeśli nie wyrażasz zgody, ustawienia dotyczące plików cookies możesz zmienić w swojej przeglądarce.</p>';
-    hulkContainer.setAttribute('id', 'hulk_container');
-
-    link.setAttribute('href', '#');
-    link.setAttribute('title', 'Zamknij');
+    hulkContainer.id = 'hulk_container';
+    link.href = '#';
+    link.title = 'Zamknij';
     link.innerHTML = 'x';
 
     function clickHandler(e) {
-        if (e.preventDefault) {
-            e.preventDefault();
-        } else {
-            e.returnValue = false;
-        }
 
         document.body.removeChild(hulkContainer);
         document.body.removeChild(container);
-
+        localStorage.setItem('marvel-cookies', '1');
     }
 
-    if (link.addEventListener) {
-        link.addEventListener('click', clickHandler);
-    } else {
-        link.attachEvent('onclick', clickHandler);
-    }
+    link.addEventListener('click', clickHandler);
 
     container.appendChild(link);
     document.body.appendChild(container);
     document.body.appendChild(hulkContainer);
 
     return true;
-})();
+};
+
+if (!localStorage.getItem('marvel-cookies')) {
+    cookiesInitialize();
+}
