63295404fd29045f31f96ac5531d96b7717dfa68
piotrnowoslawski <jdm-option@jdm-option.pl>
2018-06-16 13:20:39 +0200

    active nav section in progress

--- assets/js/script_navigation_active.js
+++ assets/js/script_navigation_active.js
@@ -1,23 +1,44 @@
 (function () {
-    var navSections = document.querySelectorAll('section');
-    var navItems = document.querySelectorAll('li');
-    var navBar = document.querySelector('.navigation');
+    var allAnchors = document.querySelectorAll('[href^=#]');
+    var selector = anchor.getAttribute('href');
+    var getElemDistances = [];
+    var offsets = [];
 
-    function getElemDistance(navSections) {
+    allAnchors.forEach(function () {
+        anchor.getAttribute('href');
+        var getElemDistance = document.querySelector(selector);
+        getElemDistances.push(getElemDistance);
+    });
+
+
+    offsets = getElemDistances.map(function (getElemDistance) {
+        return getElemDistance.offsetTop;
+    });
+
+
+
+    window.addEventListener('scroll', function() {
+        var offset = window.pageYOffset;
+        getElemDistances.forEach(function (getElemDistance, index) {
+            var classList = allAnchors[index].classList;
+            if (getElemDistance.offsetTop <= offset) {
+              classList.add('active');
+            } else {
+                classList.remove('active');
+            }
+        })
+    });
+
+    function getElemDistance( elem ) {
         var location = 0;
-        if (navSections.offsetParent) {
+        if (elem.offsetParent) {
             do {
-                location += navSections.offsetTop;
-                navSections = navSections.offsetParent;
-            } while (navSections);
+                location += elem.offsetTop;
+                elem = elem.offsetParent;
+            } while (elem);
         }
         return location >= 0 ? location : 0;
     }
 
-
-    function navHighlight() {
-
-    }
-    navHighlight();
 
 }());
