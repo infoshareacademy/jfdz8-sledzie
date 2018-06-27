67b8a52dfe8f0bc3788482e6a9067e271375d90a
Agata Budzinska <agatabudzinska@gmail.com>
2018-06-27 20:00:13 +0200

    working on team section animation on scroll

---
+++ assets/js/script_team_scroll.js
@@ -0,0 +1,14 @@
+var team = document.querySelector("#team");
+var teamPhotos = document.querySelectorAll(".team__photo");
+var topDistance = team.offsetTop;
+console.log(topDistance);
+
+window.addEventListener("scroll", sectionAnimation);
+
+function sectionAnimation () {
+    if (windowDistance === topDistance) {
+        teamPhotos.forEach(function (element) {
+            element.classList.add("active--agata", "active--piotrek", "active--magda")
+        })
+    }
+}
