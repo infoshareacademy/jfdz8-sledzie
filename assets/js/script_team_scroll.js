65d434f2241361690856b2a349cc81c1754b72f5
Agata Budzinska <agatabudzinska@gmail.com>
2018-06-28 23:10:50 +0200

    add test effect on each team member photo

--- assets/js/script_team_scroll.js
+++ assets/js/script_team_scroll.js
@@ -1,17 +1,22 @@
-var photo = $(".team__photo")
+var photo = Array.from($(".team__photo"));
 var teamSection = $(".team").offset().top;
 var windowPosition;
 var teamPosition;
+
 
 $(window).on('scroll', function() {
     windowPosition = window.pageYOffset;
     teamPosition = teamSection;
 
     if(windowPosition > teamPosition) {
-        photo.addClass("active__team");
+        $(photo[0]).addClass("active__team--magda") &&
+        $(photo[1]).addClass("active__team--agata") &&
+        $(photo[2]).addClass("active__team--piotrek");
     }
     else {
-        photo.removeClass("active__team");
+        $(photo[0]).removeClass("active__team--magda") &&
+        $(photo[1]).removeClass("active__team--agata") &&
+        $(photo[2]).removeClass("active__team--piotrek");
     }
 });
 
