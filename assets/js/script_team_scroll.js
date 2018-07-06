914269cf7db4a101c3b659723bf9e22212fdd7ad
Agata Budzinska <agatabudzinska@gmail.com>
2018-07-06 21:45:53 +0200

    code refactored v.2

--- assets/js/script_team_scroll.js
+++ assets/js/script_team_scroll.js
@@ -1,4 +1,4 @@
-var $photo = Array.from($(".team__photo"));
+var photo = Array.from($(".team__photo"));
 var $teamSection = $(".team").offset().top;
 var $teamMemberName = $(".team__name");
 var $socialSection = $(".team__social");
@@ -11,17 +11,17 @@ $(window).on("scroll", function() {
     teamPosition = $teamSection;
 
     if(windowPosition > teamPosition) {
-        ($($photo[0]).css("backgroundImage","url(assets/img/magda_img.jpg)") &&
-        $($photo[1]).css("backgroundImage","url(assets/img/agata_img.jpg)") &&
-        $($photo[2]).css("backgroundImage","url(assets/img/piotrek_img.jpg)")) &&
+        ($(photo[0]).css("backgroundImage","url(assets/img/magda_img.jpg)") &&
+        $(photo[1]).css("backgroundImage","url(assets/img/agata_img.jpg)") &&
+        $(photo[2]).css("backgroundImage","url(assets/img/piotrek_img.jpg)")) &&
         $teamMemberName.addClass("width");
 
        }
 
     else {
-        ($($photo[0]).css("backgroundImage","url(assets/img/Magda.png)") &&
-        $($photo[1]).css("backgroundImage","url(assets/img/Agata.png)") &&
-        $($photo[2]).css("backgroundImage","url(assets/img/Piotrek.png)")) &&
+        ($(photo[0]).css("backgroundImage","url(assets/img/Magda.png)") &&
+        $(photo[1]).css("backgroundImage","url(assets/img/Agata.png)") &&
+        $(photo[2]).css("backgroundImage","url(assets/img/Piotrek.png)")) &&
         $teamMemberName.removeClass("width");
     }
 });
